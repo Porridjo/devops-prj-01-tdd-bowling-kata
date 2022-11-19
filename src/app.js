67538639c23d36ac799227cf0d29e674f8626460
Rayure <Den.nguyen69@gmail.com>
2022-11-19 22:42:53 +0100

    refactor(app): lowering complexity of score method

--- src/app.js
+++ src/app.js
@@ -20,18 +20,17 @@ const score = () => {
     let score = 0;
     let scoreIndex = 0;
     for (let turn = 0; turn < 10; turn++) {
-        if (scores[scoreIndex] === 10) {
-          score += 10 + scores[scoreIndex + 1] + scores[scoreIndex + 2];
-          scoreIndex++;
-        }
-        else {
-          scoreOfTheTurn = scores[scoreIndex] + scores[scoreIndex + 1];
-          if (isSpare(scoreOfTheTurn)) {
-              score += spareBonus(scoreIndex);
-          } else {
-              score += scoreOfTheTurn;
-          }
-          scoreIndex += 2;
+        if (isStrike(scoreIndex)) {
+            score += strikeBonus(scoreIndex);
+            scoreIndex++;
+        } else {
+            scoreOfTheTurn = scores[scoreIndex] + scores[scoreIndex + 1];
+            if (isSpare(scoreOfTheTurn)) {
+                score += spareBonus(scoreIndex);
+            } else {
+                score += scoreOfTheTurn;
+            }
+            scoreIndex += 2;
         }
     }
     return score;
@@ -47,6 +46,14 @@ const isSpare = (scoreOfTheTurn) => {
 
 const spareBonus = (scoreIndex) => {
     return 10 + scores[scoreIndex + 2];
+};
+
+const isStrike = (scoreIndex) => {
+    return scores[scoreIndex] === 10;
+};
+
+const strikeBonus = (scoreIndex) => {
+    return 10 + scores[scoreIndex + 1] + scores[scoreIndex + 2];
 };
 
 module.exports = { roll, score, resetScores };
