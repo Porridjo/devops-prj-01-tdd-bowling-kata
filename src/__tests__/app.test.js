52242d83163d68addd636a3f68717c12b5e2ed08
Rayure <Den.nguyen69@gmail.com>
2022-11-19 19:53:35 +0100

    refactor: adding rollMany function to first test case

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -2,6 +2,7 @@ const { roll, score } = require('../app.js');
 
 describe('bowling kata test suite', () => {
   it('should return 0 for a gutter game', () => {
+    rollMany(0,20);
     expect(score()).toBe(0);
   });
   it('should return 20 for a game with only 1s', () => {
