5bf3b45ab09bf8a515220f50fa4cc3679751b6a3
Rayure <Den.nguyen69@gmail.com>
2022-11-19 21:49:29 +0100

    test: adding spare test case

--- src/__tests__/app.test.js
+++ src/__tests__/app.test.js
@@ -1,18 +1,30 @@
-const { roll, score } = require('../app.js');
+const { roll, score, resetScores } = require('../app.js');
+
+beforeEach(() => {
+    resetScores();
+});
 
 describe('bowling kata test suite', () => {
-  it('should return 0 for a gutter game', () => {
-    rollMany(0,20);
-    expect(score()).toBe(0);
-  });
-  it('should return 20 for a game with only 1s', () => {
-    rollMany(1,20);
-    expect(score()).toBe(20);
-  });
+    it('should return 0 for a gutter game', () => {
+        rollMany(0, 20);
+        expect(score()).toBe(0);
+    });
+    it('should return 20 for a game with only 1s', () => {
+        rollMany(1, 20);
+        expect(score()).toBe(20);
+    });
+    it('should return 16 for a game with a spare followed by 3 and 17 gutter balls', () => {
+        roll(5);
+        roll(5);
+        roll(3);
+        rollMany(0, 17);
+
+        expect(score()).toBe(16);
+    });
 });
 
 const rollMany = (pins, rolls) => {
-  for (let i=0; i<rolls; i++) {
-    roll(pins);
-  }
-}
+    for (let i = 0; i < rolls; i++) {
+        roll(pins);
+    }
+};
--- src/app.js
+++ src/app.js
@@ -7,16 +7,32 @@
 
 10/- 1/1 -/- -/- -/- -/- -/- -/- -/- -/- = 14
 
-10 10 10 10 10 10 10 10 10 10 = 300
+10/- 10/- 10/- 10/- 10/- 10/- 10/- 10/- 10/- 10/- = 300
 */
-let currentScore = 0;
+
+let scores = [];
+
+const resetScores = () => {
+    scores = [];
+};
 
 const roll = (pins) => {
-  return currentScore += pins;
-}
+    scores.push(pins);
+};
 
 const score = () => {
-  return currentScore;
-}
+    let score = 0;
+    let scoreIndex = 0;
+    for (let turn = 0; turn < 10; turn++) {
+        scoreOfTheTurn = scores[scoreIndex] + scores[scoreIndex + 1];
+        if (scoreOfTheTurn == 10) {
+            score += 10 + scores[scoreIndex + 2];
+        } else {
+            score += scoreOfTheTurn;
+        }
+        scoreIndex += 2;
+    }
+    return score;
+};
 
-module.exports = { roll, score };
+module.exports = { roll, score, resetScores };
