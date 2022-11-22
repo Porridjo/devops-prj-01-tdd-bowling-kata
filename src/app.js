9e3611f663457ec3e51d91de14c400a28c4a850d
tanguy.queguineur <tanguy.queguineur@student.vinci.be>
2022-11-22 18:08:14 +0100

    test(app): adding spare test case

--- src/app.js
+++ src/app.js
@@ -1,11 +1,25 @@
-let currentScore = 0;
+let scores = [];
 
-const roll = function(pins) {
-    return (currentScore += pins);
+const resetScores = () => {
+    scores = [];
+};
+const roll = function (pins) {
+    scores.push(pins);
 };
 
 const score = function () {
-    return currentScore;
+    let score = 0;
+    let scoreIndex = 0;
+    for (let turn = 0; turn < 10; turn++) {
+        scoreOfTheTurn = scores[scoreIndex] + scores[scoreIndex + 1];
+        if (scoreOfTheTurn == 10) {
+            score += 10 + scores[scoreIndex + 2];
+        } else {
+            score += scoreOfTheTurn;
+        }
+        scoreIndex += 2;
+    }
+    return score;
 };
 
-module.exports = { roll, score };
+module.exports = { roll, score, resetScores };
